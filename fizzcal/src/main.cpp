25396145cedcd4fa36d0e290be40ee3dca34b0fe
friezi <>
2008-04-02 10:50:22 +0000

    v1.02

--- fizzcal/src/main.cpp
+++ fizzcal/src/main.cpp
@@ -28,7 +28,7 @@ using namespace cmdl;
 using namespace mexp;
 using namespace ds;
 
-const char *version="1.01";
+const char *version = "1.02";
 
 const static string formula = "formula";
 const static string commands = "commands";
